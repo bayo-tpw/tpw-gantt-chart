5677375d945d8adbcb9dc1a8aec27e85ee94468b
bayo-tpw <bayo@phoenixwaynec.org.uk>
2025-11-06 21:26:34 +0000

    Update and rename airtable.js to airtable-test.js

---
+++ pages/api/airtable-test.js
@@ -0,0 +1,7 @@
+{
+     "results": {
+       "milestones": { "success": true/false, "error": "..." },
+       "actions": { "success": true/false, "error": "..." },
+       "people": { "success": true/false, "error": "..." }
+     }
+   }
